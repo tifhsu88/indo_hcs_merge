ba968ab9bc8e2b5fd707a571461289baae1cb3ef
tifhsu88 <tiffanyhsu8@gmail.com>
2022-09-14 12:32:54 -0700

    unable to open hcsa gdb

--- code/merge_script.R
+++ code/merge_script.R
@@ -1,47 +1,23 @@
+## Merge concession and conservation area feature classes in geodatabases
 
 # load packages
 library(tidyverse)
 library(sf)
 library(here)
 
+# read simple features from files
 
-#import shapefiles as sf objects
+#this file doesn't open
+hcsa_0083 <- st_read(dsn = here("data/hcsa_0083/hcsa_digitization.gdb"))
 
-#this doesn't open
-hcsa_0083 <- st_read(dsn = here("data/hcsa_0083/hcsa_digitization.gdb")) #, layer = "conservation_areas")
-
-# this sample gdb from https://gisdata-piercecowa.opendata.arcgis.com/datasets/piercecowa::development-engineering-mobile-homes/explore works
+#this sample gdb from https://gisdata-piercecowa.opendata.arcgis.com/datasets/piercecowa::development-engineering-mobile-homes/explore opens
 sample_gdb <- st_read(dsn = here("data/sample_gdb.gdb"))
 
-
-# merge the concession boundaries and conservation areas
-
-
-#merging files into one directory
-
-# #set working directory to the one containing all the files that need to be merged,
-# 
-# # get the list of files in that directory
+# # get the list of files in the directory
 # file_list <- list.files(here("insert_directory_here"), pattern = *gdb)
-# 
 
-# #read files
+# read files
 # gdb_list <- lapply(file_list, st_read)
 
-# # merge the files into a single dataframe
-# 
-# for (file in file_list){
-#   
-#   # if the merged dataset doesn't exist, create it
-#   if (!exists("dataset")){
-#     dataset <- read.table(file, header=TRUE, sep="\t")
-#   }
-#   
-#   # if the merged dataset does exist, append to it
-#   if (exists("dataset")){
-#     temp_dataset <-read.table(file, header=TRUE, sep="\t")
-#     dataset<-rbind(dataset, temp_dataset)
-#     rm(temp_dataset)
-#   }
-#   
-# }
+# merge the files into a single dataframe
+#create for loop 
